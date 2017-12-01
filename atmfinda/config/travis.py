c3b39f818eb1b6d459e358524782fa5d154e9dd3
Osaetin Daniel <osaetindaniel@gmail.com>
2017-12-01 06:14:23 +0100

    Changed Database credentials in travis config file

--- atmfinda/config/travis.py
+++ atmfinda/config/travis.py
@@ -3,7 +3,7 @@
 DEBUG = True
 SECRET_KEY = 'mysecretkey'
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@postgres/atmfinda'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/atmfinda-test'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 GOOGLE_MAPS_KEY = ''
