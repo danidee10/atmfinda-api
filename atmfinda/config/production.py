055f491e010ba2425db0877d834491099b4910fa
Osaetin Daniel <osaetindaniel@gmail.com>
2017-12-06 22:23:32 +0100

    Added Production Config for heroku

---
+++ atmfinda/config/production.py
@@ -0,0 +1,9 @@
+from os import environ
+
+DEBUG = False
+SECRET_KEY = environ.get('SECRET_KEY')
+
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+GOOGLE_MAPS_KEY = environ.get('GOOGLE_MAPS_KEY')
