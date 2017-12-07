7e38f711d6b9d66e768e6a53c9e12acb06875c60
Osaetin Daniel <osaetindaniel@gmail.com>
2017-12-07 09:19:23 +0100

    Made Flask Admin URL an environment variable
    So we can change the admin URL in production

--- atmfinda/admin.py
+++ atmfinda/admin.py
@@ -1,11 +1,15 @@
 """Flask admin.py file."""
 
+from os import environ
 from flask_admin import Admin
 from flask_admin.contrib.geoa import ModelView
 
 from .models import db, ATM, User
 
-admin = Admin(name='atmfinda', template_mode='bootstrap3')
+admin = Admin(
+    name='atmfinda', url=environ.get('ADMIN_URL', '/admin'),
+    template_mode='bootstrap3'
+)
 
 admin.add_view(ModelView(ATM, db.session))
 admin.add_view(ModelView(User, db.session))
